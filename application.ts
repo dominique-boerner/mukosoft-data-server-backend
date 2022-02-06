df572fa0e145319d97a5b676b6f3f0b719ee0850
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-02-06 14:57:35 +0100

    add store and readme

--- application.ts
+++ application.ts
@@ -2,15 +2,19 @@ import {Application} from "./deps.ts";
 import router from "./router.ts";
 import DsddbService from "./services/dsddb-service.ts";
 
+console.debug("**** Application Start ****");
+
 const application = new Application();
 const host = "localhost";
 const port = "9000";
+
+console.debug("**** Loading stores ... ****");
 
 const dsdbbService = new DsddbService();
 await dsdbbService.getRecipeStore().load().then(() => console.debug("Recipe store loaded"));
 await dsdbbService.getMedicationStore().load().then(() => console.debug("Medication store loaded"));
 
-dsdbbService.add(dsdbbService.getRecipeStore(), "test234", { name: "asd"}).then((r) => console.log(r));
-
 application.use(router.routes());
 application.listen(`${host}:${port}`);
+
+console.debug(`**** Server ist listening on ${host}:${port} ****`);
