079578f8ef2931bd25b4ccca1c84387fe846e68f
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-02-06 15:27:39 +0100

    rename application.ts

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import DsddbService from "./services/dsddb-service.ts";
 
 console.debug("**** Application Start ****");
 
-const application = new Application();
+const index = new Application();
 const host = "localhost";
 const port = "9000";
 
@@ -14,7 +14,7 @@ const dsdbbService = new DsddbService();
 await dsdbbService.getRecipeStore().load().then(() => console.debug("Recipe store loaded"));
 await dsdbbService.getMedicationStore().load().then(() => console.debug("Medication store loaded"));
 
-application.use(router.routes());
-application.listen(`${host}:${port}`);
+index.use(router.routes());
+index.listen(`${host}:${port}`);
 
 console.debug(`**** Server ist listening on ${host}:${port} ****`);
